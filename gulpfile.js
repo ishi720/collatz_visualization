5f6cadf2a73f1ac493991cb35188e958b64581b7
ishi720 <ishizuka720@gmail.com>
2020-10-25 19:22:17 +0900

    スタイル部分をsassファイルへ移動。gulpでsassのcompileとminifyを行う。

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 'use strict';
 
 const gulp = require('gulp');
+const sass = require('gulp-sass');
+const cleanCSS  = require('gulp-clean-css');
 const rename = require('gulp-rename');
+const sourcemaps = require('gulp-sourcemaps');
 const uglify = require('gulp-uglify');
 require('gulp-watch');
 const plumber = require('gulp-plumber');
@@ -10,6 +13,19 @@ const browserSync = require('browser-sync').create();
 gulp.task('reload', (done) => {
     browserSync.reload();
     done();
+});
+
+//sassコンパイル
+gulp.task('sass', () => {
+    return gulp.src('app/css/customize.scss')
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(rename({extname: '.min.css'}))
+        .pipe(cleanCSS())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('browser-sync', () => {
@@ -31,11 +47,12 @@ gulp.task('js-minify', () => {
 
 //ファイル監視
 gulp.task('watch', () => {
+	gulp.watch('app/css/customize.scss', gulp.task('sass'));
     gulp.watch('app/index.html', gulp.task('reload'));
     gulp.watch(['./app/js/*.js', '!./app/js/*.min.js'], gulp.task('js-minify'));
 });
 
-gulp.task('deploy', gulp.series(gulp.parallel('js-minify')));
+gulp.task('deploy', gulp.series(gulp.parallel('sass','js-minify')));
 
 //デフォルト
 gulp.task('default', gulp.series('deploy', gulp.parallel('browser-sync','watch')));
